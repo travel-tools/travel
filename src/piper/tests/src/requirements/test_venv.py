a1aab61bf6a28c71491d9887ac9c4a21bd12019b
Federico Pugliese <federico.pugliese4@gmail.com>
2021-09-25 17:59:04 +0200

    Testing deep requirements

---
+++ src/piper/tests/src/requirements/test_venv.py
@@ -0,0 +1,47 @@
+import os
+
+from piper.cli.cleaner import Cleaner
+from piper.tools.venv import Virtualenv
+
+
+def test_remove_requirements(complex_project):
+
+    # Common pipe
+    pipe = complex_project.pipes[complex_project.common]
+
+    # Create the virtualenv
+    venv = Virtualenv(pipe)
+    venv.create()
+    venv.update()
+
+    # Check the requirement file
+    venv.freeze()
+    with open(pipe.requirements_file, "r") as f:
+        right_requirements = f.read()
+
+    # Install manually a requirement
+    venv.pip.run("install pandas")
+
+    # Check that this function removes the manually inserted requirements
+    venv.update()
+    venv.freeze()
+    with open(pipe.requirements_file, "r") as f:
+        now_requirements = f.read()
+    assert right_requirements == now_requirements
+
+    # Remove requirements from the pipe
+    old_requirements = pipe.requirements
+    pipe.requirements = ["pandas==1.3.3"]
+    venv.update()
+    venv.freeze()
+    with open(pipe.requirements_file, "r") as f:
+        now_requirements = f.read()
+    assert "pandas" in now_requirements
+    assert "python-dateutil" in now_requirements
+    assert "six" in now_requirements
+    for r in old_requirements:
+        assert r not in now_requirements
+
+    # Clean everything
+    Cleaner().manage_from_pipe(pipe)
+    os.remove(pipe.requirements_file)
