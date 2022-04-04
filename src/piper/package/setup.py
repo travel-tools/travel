c0baa60cddbbe2375429fb0131a45064c6f2be7a
f.pugliese <f.pugliese@reply.it>
2022-04-04 10:51:23 +0200

    Downgrade pytest version

--- src/piper/package/setup.py
+++ src/piper/package/setup.py
@@ -50,6 +50,6 @@ setup(
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7"
+        "Programming Language :: Python :: 3.6"
     ]
 )
