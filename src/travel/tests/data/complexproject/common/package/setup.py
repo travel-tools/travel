6428c45bc854d9029ccdf772826cb882536cfe04
f.pugliese <f.pugliese@reply.it>
2022-06-18 12:57:44 +0200

    Nested resources in common to test that resources can be copied

--- src/travel/tests/data/complexproject/common/package/setup.py
+++ src/travel/tests/data/complexproject/common/package/setup.py
@@ -2,6 +2,15 @@ from setuptools import setup, find_packages
 import os
 
 
+# Function to retrieve resources files
+def _get_resources(package_name):
+    # Get all the resources (also on nested levels)
+    res_paths = os.path.join(package_name, "resources")
+    all_resources = [os.path.join(folder, file) for folder, _, files in os.walk(res_paths) for file in files]
+    # Remove the prefix: start just from "resources"
+    return [resource[resource.index("resources"):] for resource in all_resources]
+
+
 # Read requirements
 requirements_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "requirements.txt")
 with open(requirements_file, "r") as f:
@@ -13,4 +22,5 @@ setup(name="common",
       version="0.0.0",
       description="A common package",
       packages=find_packages(),
+      package_data={"common": _get_resources("common")},
       install_requires=requirements)
