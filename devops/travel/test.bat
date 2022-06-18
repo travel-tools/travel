e21703d9a847eb67ac754abd720fa3913c29b29f
f.pugliese <f.pugliese@reply.it>
2022-06-18 14:00:49 +0200

    Test without using venv after no setup

--- devops/travel/test.bat
+++ devops/travel/test.bat
@@ -21,6 +21,6 @@ travel clean
 
 travel pack --no-setup sdist
 ( venv-second\Scripts\python -m second && exit 1 ) || echo No setup ok
-venv-second\Scripts\python -m pip install build\package\dist\second-0.0.0.tar.gz
-venv-second\Scripts\python -m second
+python -m pip install build\package\dist\second-0.0.0.tar.gz
+python -m second
 travel clean
