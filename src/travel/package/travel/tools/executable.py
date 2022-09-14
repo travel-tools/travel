83e077fdf4dd301afa509a8fe8e561498439263f
Andrea Migotto <a.migotto@reply.it>
2022-09-14 15:06:57 +0200

    Fix faulty handling of whitespaces in Python path

--- src/travel/package/travel/tools/executable.py
+++ src/travel/package/travel/tools/executable.py
@@ -10,7 +10,7 @@ class Executable(Shell):
 
     def __init__(self, tool: str, pre_command: str = None):
         super().__init__(pre_command)
-        self._tool = tool
+        self._tool = "{tool}"
 
     def run(self, command: str, capture: bool = False, text: bool = True, cwd: str = None) -> subprocess.CompletedProcess:
         return super().run(f"{self._tool} {command}", capture=capture, text=text, cwd=cwd)
