3f8ba7179404c2cf1fdf32a8f93bda080affe1f0
amigotto <andrea.mig8@gmail.com>
2022-09-14 18:05:32 +0200

    Fix string interpolation for the filepath

--- src/travel/package/travel/tools/python.py
+++ src/travel/package/travel/tools/python.py
@@ -8,7 +8,8 @@ from travel.tools.executable import Executable
 class Python(Executable):
 
     def __init__(self, path: str = None, pre_command: str = None):
-        self.path = f'"{path}"' or f'"{os.environ.get("TRAVEL_PYTHON_PATH", sys.executable)}"'
+        path = path or os.environ.get("TRAVEL_PYTHON_PATH", sys.executable)
+        self.path = f'"{path}'
         super().__init__(self.path, pre_command=pre_command)
         # TODO version from Popen
 
