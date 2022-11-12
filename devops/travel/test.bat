646373e058c6fb4a8d0cdf0d797603deb247f015
Federico Pugliese <f.pugliese@reply.it>
2022-11-12 18:16:46 +0100

    Test casing python version

--- devops/travel/test.bat
+++ devops/travel/test.bat
@@ -12,12 +12,21 @@ cd ..\..\
 cd complexproject
 travel --debug clean
 
+:: Python version
+:: no compatible python
 echo python: 2.7.1 > bag.yml
-( travel setup && exit 1 ) || echo Wrong python ok
+( travel setup && exit 1 ) || echo No python ok
 
+:: wrong config (2.7.1 pointing to python 3)
+for /F "tokens=*" %%p IN ('python -c "import sys; print(sys.executable)"') DO @(travel config add python 2.7.1 %%p)
+( travel setup && exit 1 ) || echo Wrong python version ok
+
+:: right config
 python -c "import sys; print('python: ' + sys.version.split(' ')[0])" > bag.yml
-
+for /F "tokens=*" %%p IN ('python -c "import sys; print(sys.version.split()[0], sys.executable)"') DO @(travel config add python %%p)
 travel --debug setup
+:: End Python version
+
 cd microservices\second
 venv-second\Scripts\python -m second
 
--- devops/travel/test.sh
+++ devops/travel/test.sh
@@ -12,12 +12,21 @@ cd ../../
 cd complexproject
 travel --debug clean
 
+# Python version
+# no compatible python
 echo "python: 2.7.1" > bag.yml
-( travel setup && exit 1 ) || echo Wrong python ok
+( travel setup && exit 1 ) || echo No python ok
 
+# wrong config (2.7.1 pointing to python 3)
+travel config add python 2.7.1 $( python -c 'import sys; print(sys.executable)' )
+( travel setup && exit 1 ) || echo Wrong python version ok
+
+# right config
 python -c 'import sys; print("python: " + sys.version.split(" ")[0])' > bag.yml
-
+travel config add python $( python -c 'import sys; print(sys.version.split()[0], sys.executable)' )
 travel --debug setup
+# End Python version
+
 cd microservices/second/
 venv-second/bin/python -m second
 
